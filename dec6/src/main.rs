6b3ca30f35b7c444774e3c8c292b39fb21bdac16
Jim McGinley <jtmcginleyiii@gmail.com>
2025-12-06 08:32:28 -0500

    dec 5 part 2, more hardcoded than i'd like

--- dec6/src/main.rs
+++ dec6/src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let input = read_file("src/input/test.txt");
+    let input = read_file("src/input/input.txt");
     let (number_matrix, symbol_vec) = parse_input(&input);
-    //print_problem(&number_matrix);
-    //println!("Symbols: {:?}", symbol_vec);
-    let mut solution: u64 = 0;
-    for i in 0..symbol_vec[0].len() {
-        solution += operate_column(&number_matrix, &symbol_vec[0], i);
+    print_problem(&number_matrix);
+    println!("Symbols: {:?}", symbol_vec);
+    let mut solution: i64 = 0;
+    for i in 0..symbol_vec.len() {
+        solution += operate_problem(&number_matrix, symbol_vec.clone(), i);
     }
     println!("Solution: {}", solution);
 }
@@ -15,28 +15,48 @@ fn read_file(input: &str) -> Vec<String> {
     contents.lines().map(|s| s.to_string()).collect()
 }
 
-fn print_problem(input: &Vec<Vec<u64>>) {
+fn print_problem(input: &Vec<Vec<i64>>) {
     for line in input {
         println!("{:?}", line);
     }
 }
 
-fn parse_input(input: &Vec<String>) -> (Vec<Vec<u64>>, Vec<String>){
-    let mut number_matrix: Vec<Vec<u64>> = Vec::new();
+fn parse_input(input: &Vec<String>) -> (Vec<Vec<i64>>, Vec<String>){
+    let mut number_matrix: Vec<Vec<i64>> = Vec::new();
     let mut symbol_vec: Vec<String> = Vec::new();
-    for line in input {
-        if line.chars().nth(0).unwrap() != '*' {
-            let row: Vec<u64> = line.split_whitespace().map(|s| s.parse::<u64>().unwrap()).collect();
-            number_matrix.push(row);
+    
+    let mut problem_number: usize = 0;
+    number_matrix.push(Vec::new());
+
+    for i in (0..input[0].len()).rev() {
+        
+        let next_number = format!("{}{}{}{}",
+                                  input[0].chars().nth(i).unwrap(),
+                                  input[1].chars().nth(i).unwrap(),
+                                  input[2].chars().nth(i).unwrap(),
+                                  input[3].chars().nth(i).unwrap())
+                                  .trim().to_string();
+
+        let parsed_number = next_number.parse::<i64>().unwrap_or(-1);
+        if parsed_number == -1 {
+            continue;
         } else {
-            symbol_vec.push(line.split_whitespace().collect());
+            number_matrix[problem_number].push(parsed_number);
         }
+
+        if input[4].chars().nth(i).unwrap() == '+' || input[4].chars().nth(i).unwrap() == '*' {
+            symbol_vec.push(input[4].chars().nth(i).unwrap().to_string());
+            problem_number += 1;
+            number_matrix.push(Vec::new())
+        } 
+        
     }
+
     (number_matrix, symbol_vec)
 }
 
-fn operate_column(input: &Vec<Vec<u64>>, symbol: &str, column: usize) -> u64 {
-    let mut output: u64 = input[0][column];
+fn operate_column(input: &Vec<Vec<i64>>, symbol: &str, column: usize) -> i64 {
+    let mut output: i64 = input[0][column];
     for row in input.iter().skip(1) {
         match symbol.chars().nth(column).unwrap() {
             '+' => output += row[column],
@@ -44,5 +64,21 @@ fn operate_column(input: &Vec<Vec<u64>>, symbol: &str, column: usize) -> u64 {
             _ => {}
         }
     }
+    output
+}
+
+fn operate_problem(input: &Vec<Vec<i64>>, symbol: Vec<String>, problem_number: usize) -> i64 {
+    let mut output: i64 = 0;
+    if input[problem_number].len() != 0 {
+        output = input[problem_number][0];
+        for num in input[problem_number].iter().skip(1) {
+            match symbol[problem_number].chars().nth(0).unwrap() {
+                '+' => output += num,
+                '*' => output *= num,
+                _ => {}
+            }
+        }
+    }
+    println!("Output: {} Problem Number: {}", output, problem_number);
     output
 }
